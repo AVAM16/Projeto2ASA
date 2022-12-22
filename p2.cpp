163e057c9742fcf98c5b3d5f5cad6dbd22f3c77b
Shad0wAVM <arturvasco.martins@gmail.com>
2022-12-22 11:36:05 +0000

    Leitura do Grafo

--- p2.cpp
+++ p2.cpp
@@ -1,1 +1,31 @@
+#include <vector>
+#include <iostream>
 
+using namespace std;
+
+unsigned int V, E;
+
+void addEdge(vector <pair<int, int> > adj[], int u, int v, int wt)
+{
+  adj[u].push_back(make_pair(v, wt));
+  adj[v].push_back(make_pair(u, wt));
+}
+
+void readVE(){
+  cin >> V >> E;
+}
+
+void readGraph(vector <pair<int, int> > adj[]){
+  for (int a = 0; a < E; a++){
+    int u, v, wt;
+    cin >> u >> v >> wt;
+    addEdge(adj, u, v, wt);
+  }
+}
+
+int main(){
+  readVE();
+  vector <pair<int, int> > adj[V];
+  readGraph(adj);
+  return 0;
+}
