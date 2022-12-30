604a60849c9c49d784745a7d150f60e2fd800b80
Shad0wAVM <arturvasco.martins@gmail.com>
2022-12-30 14:58:21 +0000

    Algumas alteracoes e otimizacoes

--- p2.cpp
+++ p2.cpp
@@ -1,4 +1,4 @@
-#include<algorithm>
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <array>
@@ -8,29 +8,29 @@ using namespace std;
 
 class MakeSet
 {
-	int *parent;
+	int *predecessor;
 	int *rank;
 
 public:
 	MakeSet(int n)
 	{
-		parent = new int[n];
+		predecessor = new int[n];
 		rank = new int[n];
 
 		for (int i = 0; i < n; i++)
 		{
-			parent[i] = -1;
+			predecessor[i] = -1;
 			rank[i] = 1;
 		}
 	}
 
 	int FindSet(int i)
 	{
-		if (parent[i] == -1)
+		if (predecessor[i] == -1)
 		{
 			return i;
 		}
-		return parent[i] = FindSet(parent[i]);
+		return predecessor[i] = FindSet(predecessor[i]);
 	}
 
 	void Union(int u, int v)
@@ -42,15 +42,15 @@ public:
 		{
 			if (rank[s1] < rank[s2])
 			{
-				parent[s1] = s2;
+				predecessor[s1] = s2;
 			}
 			else if (rank[s1] > rank[s2])
 			{
-				parent[s2] = s1;
+				predecessor[s2] = s1;
 			}
 			else
 			{
-				parent[s2] = s1;
+				predecessor[s2] = s1;
 				rank[s1] += 1;
 			}
 		}
@@ -59,7 +59,7 @@ public:
 
 class Graph
 {
-	vector<array<int, 3>> edgelist;
+	vector<array<int, 3>> edges;
 	int V;
 
 public:
@@ -70,16 +70,29 @@ public:
 
 	void addEdge(int u, int v, int w)
 	{
-		edgelist.push_back({u, v, -w});
+		edges.push_back({u, v, -w});
+	}
+
+	void fillGraph(int e)
+	{
+		for (int a = 0; a < e; a++)
+		{
+			int u, v, w;
+			if (scanf("%d%d%d", &u, &v, &w) == 0)
+			{
+				break;
+			}
+			addEdge(u, v, w);
+		}
 	}
 
 	int MSTKruskal()
 	{
-		sort(edgelist.begin(), edgelist.end(), [](const array<int, 3> &w1, const array<int, 3> &w2)
+		sort(edges.begin(), edges.end(), [](const array<int, 3> &w1, const array<int, 3> &w2)
 				 { return w1[2] < w2[2]; });
-		MakeSet s(V+1);
+		MakeSet s(V + 1);
 		int ans = 0;
-		for (array<int, 3> edge : edgelist)
+		for (array<int, 3> edge : edges)
 		{
 			int u = edge[0];
 			int v = edge[1];
@@ -95,26 +108,21 @@ public:
 	}
 };
 
-Graph fillGraph(int v, int e)
-{
-	Graph g(v);
-	for (int a = 0; a < e; a++)
-	{
-		int u, v, w;
-		scanf("%d%d%d", &u,&v,&w);
-		g.addEdge(u, v, w);
-	}
-	return g;
-}
-
 int main()
 {
 	int V;
 	int E;
-	scanf("%d", &V);
-	scanf("%d", &E);
-	Graph G = fillGraph(V, E);
+	if (scanf("%d", &V) == 0)
+	{
+		return 0;
+	}
+	if (scanf("%d", &E) == 0)
+	{
+		return 0;
+	}
+	Graph G(V);
+	G.fillGraph(E);
 	int ans = G.MSTKruskal();
-	cout << abs(ans) << endl;
+	printf("%d\n", abs(ans));
 	return 0;
 }
